7672f53a1fe29f625c0fecdacb89d7993595c44e
misterhtmlcss <misterhtmlcss@gmail.com>
2022-01-16 20:34:00 -0700

    added user + post; model, schema and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  resources :posts
+
+  # 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
