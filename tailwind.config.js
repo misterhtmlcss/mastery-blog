08fff960eb281c49efc2c78df844b124edf4f9a3
misterhtmlcss <misterhtmlcss@gmail.com>
2022-01-20 19:13:02 -0700

    setup tailwind and visual test for deployment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
