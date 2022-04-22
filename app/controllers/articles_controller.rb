1a964a8abd52c584a81e9c30f528847c5b8c4d27
Roger Kondrat <misterhtmlcss@gmail.com>
2022-04-22 14:28:56 -0600

    added CRUD functionality to Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,26 +4,46 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    render html: "show"
+    @article = Article.find(params[:id])
   end
 
   def new
-    render html:"new"
+    @article = Article.new
   end
 
   def create
-    render html:"create"
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   def edit
-    render html:"edit"
+    @article = Article.find(params[:id])
   end
 
   def update
-    render html: "update"
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    render html:"destroy"
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
