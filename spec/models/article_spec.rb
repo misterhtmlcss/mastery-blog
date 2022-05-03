d921517b734508f150a61975574eded3e4664f2b
Roger Kondrat <misterhtmlcss@gmail.com>
2022-05-02 21:59:54 -0600

    added first test and setup faker

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  context "validation tests" do
+    it "accepts title and body" do
+      article = Article.create(title: "test", body: "testing")
+      expect(article.title).to eq("test")
+      expect(article.body).to eq("testing")
+    end
+  end
+end
+
+
