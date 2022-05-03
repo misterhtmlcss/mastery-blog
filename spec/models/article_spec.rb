c4f7fdaaf7b4b287df13a165297e7085787e9b92
Roger Kondrat <misterhtmlcss@gmail.com>
2022-05-02 18:57:52 -0600

    went back to where the cause seemed to originate

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 RSpec.describe Article, type: :model do
   context "validation tests" do
-    # let(:article) { build(:article) }
-    # it 'returns a title and body of an article' do 
-    #   article.title = nil
-    #   expect(article.save).to eq(false)
-    # end
+  # let(:article) { build(:article) }
+    it 'returns a title and body of an article' do 
+      article = FactoryBot.build(:article)
+      expect(article).to be_valid
+    end
     
   end
 
