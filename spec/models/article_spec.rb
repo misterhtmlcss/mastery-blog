a89176989609367eb77ce222ebebad730bbed32e
Roger Kondrat <misterhtmlcss@gmail.com>
2022-05-02 22:43:18 -0600

    updated .rspec to see if its a require problem

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   context "validation tests" do
+    it "is valid with valid attributes" do
+      expect(Article.new).to be_valid
+    end
+    
     it "accepts title and body" do
       article = Article.create(title: "test", body: "testing")
       expect(article.title).to eq("test")
